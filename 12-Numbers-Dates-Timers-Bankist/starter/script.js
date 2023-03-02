2ad911ca92835914e78bed9c1dd604612e9a9617
Pedro Perez <pperezi@bch.bancodechile.cl>
2023-03-02 11:51:52 -0300

    se agrega avances

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +(inputLoginPin.value)) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +(inputTransferAmount.value);
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,20 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// convertimos string en numeros
+console.log(Number('23'));
+console.log(+'23')
+
+// Parsing
+console.log(Number.parseInt('30px'));
+
+console.log(Math.sqrt(25), 'raiz de');
+
+console.log(25 ** (1 / 2))
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (event) {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(
+  btn => btn.addEventListener('click', openModal)
+);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,136 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Selecting Elements
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics.';
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => {
+    message.remove();
+  }
+)
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '100%';
+
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+message.style.height = 
+  Number.parseFloat(getComputedStyle(message).height,
+   10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Atributes
+const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+logo.alt = 'Beatiful minimalist logo';
+
+// Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Banklist');
+
+// const link = document.querySelector('.nav__link--btn');
+
+// console.log('logo.src');
+// console.log(logo.getAttribute('src'));
+
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
+
+// Classes
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c', 'j');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// Don't use
+// logo.clasName = 'Jonas';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', (e) => {
+  const s1corrds = section1.getBoundingClientRect();
+  console.log(s1corrds);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  )
+  // Scrolling
+  // window.scrollTo(
+  //   s1corrds.left + window.pageXOffset, 
+  //   s1corrds.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1corrds.left + window.pageXOffset,
+  //   top: s1corrds.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+})
+
+
+// types of events
+const h1 = document.querySelector('h1');
+
+const alertH1 = (e) => {
+  alert('addEventListener');
+  h1.removeEventListener('mouseenter', alertH1);
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => 
+  `rgb(${randomInt(0, 255)},${randomColor(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
